174a27ef96933ace89d8e8b12d71a7a73a93409f
Naveensrinivasan07 <naveenkalz988@gmail.com>
2023-10-20 10:59:02 +0530

    required code were included

---
+++ src/SignupComponent/SignupComponent.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const SignupComponent = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup submitted:', { email, password });
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Email Id:</label>
+        <input type="email" value={email} onChange={handleEmailChange} required />
+        <br />
+        <label>New Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+        <br />
+        <br />
+        <label>Confirm Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+    
+  );
+};
+
+export default SignupComponent;
